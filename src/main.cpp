1a56a7131d82bc9a668343356886744ab4520e5b
Cameron Bannasch <cameron.bannasch@gmail.com>
2025-07-11 23:00:01 -0700

    Finish serial driver for motor

--- src/main.cpp
+++ src/main.cpp
@@ -2,109 +2,119 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 
-/* This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
-   which provides a common 'type' for sensor data and some helper functions.
+const unsigned short crc16_tab[] = { 0x0000, 0x1021, 0x2042, 0x3063, 0x4084,
+    0x50a5, 0x60c6, 0x70e7, 0x8108, 0x9129, 0xa14a, 0xb16b, 0xc18c, 0xd1ad,
+    0xe1ce, 0xf1ef, 0x1231, 0x0210, 0x3273, 0x2252, 0x52b5, 0x4294, 0x72f7,
+    0x62d6, 0x9339, 0x8318, 0xb37b, 0xa35a, 0xd3bd, 0xc39c, 0xf3ff, 0xe3de,
+    0x2462, 0x3443, 0x0420, 0x1401, 0x64e6, 0x74c7, 0x44a4, 0x5485, 0xa56a,
+    0xb54b, 0x8528, 0x9509, 0xe5ee, 0xf5cf, 0xc5ac, 0xd58d, 0x3653, 0x2672,
+    0x1611, 0x0630, 0x76d7, 0x66f6, 0x5695, 0x46b4, 0xb75b, 0xa77a, 0x9719,
+    0x8738, 0xf7df, 0xe7fe, 0xd79d, 0xc7bc, 0x48c4, 0x58e5, 0x6886, 0x78a7,
+    0x0840, 0x1861, 0x2802, 0x3823, 0xc9cc, 0xd9ed, 0xe98e, 0xf9af, 0x8948,
+    0x9969, 0xa90a, 0xb92b, 0x5af5, 0x4ad4, 0x7ab7, 0x6a96, 0x1a71, 0x0a50,
+    0x3a33, 0x2a12, 0xdbfd, 0xcbdc, 0xfbbf, 0xeb9e, 0x9b79, 0x8b58, 0xbb3b,
+    0xab1a, 0x6ca6, 0x7c87, 0x4ce4, 0x5cc5, 0x2c22, 0x3c03, 0x0c60, 0x1c41,
+    0xedae, 0xfd8f, 0xcdec, 0xddcd, 0xad2a, 0xbd0b, 0x8d68, 0x9d49, 0x7e97,
+    0x6eb6, 0x5ed5, 0x4ef4, 0x3e13, 0x2e32, 0x1e51, 0x0e70, 0xff9f, 0xefbe,
+    0xdfdd, 0xcffc, 0xbf1b, 0xaf3a, 0x9f59, 0x8f78, 0x9188, 0x81a9, 0xb1ca,
+    0xa1eb, 0xd10c, 0xc12d, 0xf14e, 0xe16f, 0x1080, 0x00a1, 0x30c2, 0x20e3,
+    0x5004, 0x4025, 0x7046, 0x6067, 0x83b9, 0x9398, 0xa3fb, 0xb3da, 0xc33d,
+    0xd31c, 0xe37f, 0xf35e, 0x02b1, 0x1290, 0x22f3, 0x32d2, 0x4235, 0x5214,
+    0x6277, 0x7256, 0xb5ea, 0xa5cb, 0x95a8, 0x8589, 0xf56e, 0xe54f, 0xd52c,
+    0xc50d, 0x34e2, 0x24c3, 0x14a0, 0x0481, 0x7466, 0x6447, 0x5424, 0x4405,
+    0xa7db, 0xb7fa, 0x8799, 0x97b8, 0xe75f, 0xf77e, 0xc71d, 0xd73c, 0x26d3,
+    0x36f2, 0x0691, 0x16b0, 0x6657, 0x7676, 0x4615, 0x5634, 0xd94c, 0xc96d,
+    0xf90e, 0xe92f, 0x99c8, 0x89e9, 0xb98a, 0xa9ab, 0x5844, 0x4865, 0x7806,
+    0x6827, 0x18c0, 0x08e1, 0x3882, 0x28a3, 0xcb7d, 0xdb5c, 0xeb3f, 0xfb1e,
+    0x8bf9, 0x9bd8, 0xabbb, 0xbb9a, 0x4a75, 0x5a54, 0x6a37, 0x7a16, 0x0af1,
+    0x1ad0, 0x2ab3, 0x3a92, 0xfd2e, 0xed0f, 0xdd6c, 0xcd4d, 0xbdaa, 0xad8b,
+    0x9de8, 0x8dc9, 0x7c26, 0x6c07, 0x5c64, 0x4c45, 0x3ca2, 0x2c83, 0x1ce0,
+    0x0cc1, 0xef1f, 0xff3e, 0xcf5d, 0xdf7c, 0xaf9b, 0xbfba, 0x8fd9, 0x9ff8,
+    0x6e17, 0x7e36, 0x4e55, 0x5e74, 0x2e93, 0x3eb2, 0x0ed1, 0x1ef0 };
 
-   To use this driver you will also need to download the Adafruit_Sensor
-   library and include it in your libraries folder.
-
-   You should also assign a unique ID to this sensor for use with
-   the Adafruit Sensor API so that you can identify this particular
-   sensor in any data logs, etc.  To assign a unique ID, simply
-   provide an appropriate value in the constructor below (12345
-   is used by default in this example).
-
-   Connections
-   ===========
-   Connect SCL to analog 5
-   Connect SDA to analog 4
-   Connect VDD to 3.3-5V DC
-   Connect GROUND to common ground
- 
-   History
-   =======
-   2015/MAR/03  - First release (KTOWN)
-*/
+typedef enum {
+    COMM_FW_VERSION = 0,
+    COMM_JUMP_TO_BOOTLOADER,
+    COMM_ERASE_NEW_APP,
+    COMM_WRITE_NEW_APP_DATA,
+    COMM_GET_VALUES, // Get motor operating parameters
+    COMM_SET_DUTY, // Motor operates in duty cycle mode
+    COMM_SET_CURRENT, // Motor operates in current loop mode
+    COMM_SET_CURRENT_BRAKE, // Motor operates in current brake mode
+    COMM_SET_RPM, // Motor operates in speed loop mode
+    COMM_SET_POS, // Motor operates in position loop mode
+    COMM_SET_HANDBRAKE, // Motor operates in handbrake current loop mode
+    COMM_SET_DETECT, // Motor real-time feedback current position command
+    COMM_ROTOR_POSITION=22,// Motor feedback current position
+    COMM_GET_VALUES_SETUP=50 , // Motor single or multiple parameter acquisition command
+    COMM_SET_POS_SPD=91, // Motor operates in position-speed loop mode
+    COMM_SET_POS_MULTI=92, // Set motor motion to single-turn mode
+    COMM_SET_POS_SINGLE=93, // Set motor motion to multi-turn mode, range ±100 turns
+    COMM_SET_POS_UNLIMITED=94, // Reserved
+    COMM_SET_POS_ORIGIN=95, // Set motor origin
+} COMM_PACKET_ID;
 
 uint16_t SAMPLERATE_DELAY_MS = 100;
+long MOTOR_SERIAL_BAUD_RATE = 9600;
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire);
 
 void setup(void)
 {
     Serial.begin(115200);
+    Serial1.begin(9600);
 
-    while (!Serial) delay(10);  // wait for serial port to open!
+    while (!Serial) delay(10);
 
-    Serial.println("Orientation Sensor Test"); Serial.println("");
-
-    /* Initialise the sensor */
     if (!bno.begin())
     {
-        /* There was a problem detecting the BNO055 ... check your connections */
-        Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+        Serial.print("no BNO055 detected");
         while (1);
     }
 
     delay(1000);
 }
 
-void printEvent(sensors_event_t* event) {
-  double x = -1000000, y = -1000000 , z = -1000000; //dumb values, easy to spot problem
-  if (event->type == SENSOR_TYPE_ACCELEROMETER) {
-    Serial.print("Accl:");
-    x = event->acceleration.x;
-    y = event->acceleration.y;
-    z = event->acceleration.z;
-  }
-  else if (event->type == SENSOR_TYPE_ORIENTATION) {
-    Serial.print("Orient:");
-    x = event->orientation.x;
-    y = event->orientation.y;
-    z = event->orientation.z;
-  }
-  else if (event->type == SENSOR_TYPE_MAGNETIC_FIELD) {
-    Serial.print("Mag:");
-    x = event->magnetic.x;
-    y = event->magnetic.y;
-    z = event->magnetic.z;
-  }
-  else if (event->type == SENSOR_TYPE_GYROSCOPE) {
-    Serial.print("Gyro:");
-    x = event->gyro.x;
-    y = event->gyro.y;
-    z = event->gyro.z;
-  }
-  else if (event->type == SENSOR_TYPE_ROTATION_VECTOR) {
-    Serial.print("Rot:");
-    x = event->gyro.x;
-    y = event->gyro.y;
-    z = event->gyro.z;
-  }
-  else if (event->type == SENSOR_TYPE_LINEAR_ACCELERATION) {
-    Serial.print("Linear:");
-    x = event->acceleration.x;
-    y = event->acceleration.y;
-    z = event->acceleration.z;
-  }
-  else if (event->type == SENSOR_TYPE_GRAVITY) {
-    Serial.print("Gravity:");
-    x = event->acceleration.x;
-    y = event->acceleration.y;
-    z = event->acceleration.z;
-  }
-  else {
-    Serial.print("Unk:");
-  }
-
-  Serial.print("\tx= ");
-  Serial.print(x);
-  Serial.print(" |\ty= ");
-  Serial.print(y);
-  Serial.print(" |\tz= ");
-  Serial.println(z);
+uint16_t calculate_checksum(unsigned char *buf, unsigned int len) {
+    unsigned int i;
+    uint16_t cksum = 0;
+    for (i = 0; i < len; i++) {
+        cksum = crc16_tab[(((cksum >> 8) ^ *buf++) & 0xFF)] ^ (cksum << 8);
+    }
+    return cksum;
 }
 
+void set_position(int32_t commanded_position_degrees) {
+
+    int32_t commanded_position = commanded_position_degrees * 1000000;
+
+    uint8_t to_write[100] = {0};
+
+    to_write[0] = 0x02; // Frame identifier
+    to_write[1] = 1 + 4; // Data length
+    to_write[2] = COMM_SET_POS; // Data identifier
+    
+    to_write[3] = ((commanded_position >> 24) & 0xFF); // bits 25–32
+    to_write[4] = ((commanded_position >> 16) & 0xFF); // bits 17–24
+    to_write[5] = ((commanded_position >> 8)  & 0xFF); // bits 9–16
+    to_write[6] = (commanded_position & 0xFF); // bits 1-8
+    // printf("%04x", calculate_checksum(to_write + 2, 5));
+
+    to_write[7] = calculate_checksum(to_write + 2, 5) >> 8;
+    to_write[8] = calculate_checksum(to_write + 2, 5) & 0xFF;
+
+    to_write[9] = 0x03;
+
+    // printf("Sending: ");
+    for (int i = 0; i<10; i++) {
+        // printf("%02x ", to_write[i]);
+        Serial1.print(to_write[i]); // Send command to motor via serial
+    }
+    // printf("\n");
+
+
+
+}
 
 void loop(void)
 {
@@ -127,6 +137,9 @@ void loop(void)
     Serial.print("Gradient");
     Serial.print("direction of gradient=");
     Serial.print(atan2(orientationData.orientation.y, orientationData.orientation.x) * 180 / 3.1415926382);
+
+    // Writing orientation code
+
 
 
     delay(SAMPLERATE_DELAY_MS);
